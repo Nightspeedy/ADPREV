42d572f3f256b86607bdf6dd8acd54e4b6a74122
Nightspeedy <b-versteegh@hotmail.nl>
2018-11-11 15:05:51 +0100

    final fixes

--- commands/premium.js
+++ commands/premium.js
@@ -6,11 +6,21 @@ module.exports.run = async(bot, message, args) => {
     
     if (message.author.id == 365452203982323712){
 
-        if (!args[0]) return message.reply()
+        if (!args[0]) return message.reply("Error! Please use at least 1 argument!")
+        
+        if (args[0] == "true"){
+            guildSettings[message.guild.id].isPremium = true;
+            message.channel.send("This server is now using ADPREV Premium.");
+        } else if (args[0] == "false") {
+            guildSettings[message.guild.id].isPremium = false;
+            message.channel.send("This server is no longer using ADPREV Premium.");
+        }
 
     } else {
         return message.channel.send("**Error!** You do not have permission to execute this command!");
     }
+
+    save();
 
 }
 module.exports.help = {
